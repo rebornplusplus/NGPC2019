6982f171023d823458911dfef44307f92e5b8284
rebornplusplus <rebornplusplus@gmail.com>
2019-11-26 01:29:23 +0600

    Create Judge Solution.cc

---
+++ J. Let's See!/Judge Solution.cc
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>`
+
+#define slld(longvalue) scanf("%lld", &longvalue)
+#define plld(longvalue) printf("%lld\n", longvalue)
+
+#define slf(longvalue) scanf("%lf", &longvalue)
+#define plf(longvalue) printf("%lf\n", longvalue)
+#define sc(letter) scanf("%c", &letter)
+#define pc(letter) printf("%c", letter)
+
+#define ss(name) scanf("%s", name)
+#define ps(name) printf("%s", name)
+
+#define pnew printf("\n")
+
+#define ll long long
+#define ull unsigned long long
+#define pll pair < long long, long long >
+#define pii pair < int, int >
+
+#define printcase(indexing,ans) printf("Case %lld: %lld\n", indexing, ans)
+
+#define pb(x) push_back(x)
+
+#define bug printf("BUG\n")
+
+#define mxlld LLONG_MAX
+#define mnlld -LLONG_MAX
+
+#define mxd 2e8
+#define mnd -2e8
+
+#define pi 3.14159265359
+
+#define mod 1000000009
+
+
+using namespace std;
+
+bool check(ll n, ll pos)
+{
+    return n & (1LL << pos);
+}
+
+ll Set(ll n, ll pos)
+{
+    return n = n | (1LL << pos);
+}
+
+int main()
+{
+    ll i, j, k, l, m, n, o, r, q;
+    ll testcase;
+    ll input, flag, tag, ans;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+    cin >> testcase;
+
+    for(j = 1; j <= testcase; j++)
+    {
+        ll temp = 1;
+
+        ans = 0;
+
+        cin >> l >> r;
+
+        if(l > r) swap(l, r);
+
+        for(i = 1; i <= 64; i++)
+        {
+            if(temp > r) break;
+
+            if(temp >= l) ans += temp;
+
+            temp *= 2;
+        }
+
+        cout << ans << endl;
+
+    }
+
+
+}
+
+
