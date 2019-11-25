dafa13642e16787848bcabf64fec990e8e0835de
rebornplusplus <rebornplusplus@gmail.com>
2019-11-26 01:19:56 +0600

    Create himel_cosine.cc

---
+++ F. Piece of Cake/himel_cosine.cc
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+//template<typename T>
+//using ordered_set=tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+#define MAX             100005
+#define MOD             1000000007
+#define eps             1e-6
+int fx[] =              {1,-1,0,0};
+int fy[] =              {0,0,1,-1};
+
+#define FastRead        ios_base::sync_with_stdio(0);cin.tie(0);
+#define fRead           freopen("in.txt","r",stdin);
+#define fWrite          freopen ("out.txt","w",stdout);
+
+#define LL              long long
+#define ULL              unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define PI              acos(-1.0)
+#define mk              make_pair
+#define pii             pair<int,int>
+#define pll             pair<LL,LL>
+#define all(a)          a.begin(),a.end()
+
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)   max(a,max(b,max(c,d)))
+
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+#define IT(it,x)	    for(it=x.begin();it!=x.end();it++)
+
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define Unique(a)       a.erase(unique(a.begin(),a.end()),a.end())
+#define TC              int t;cin >> t;FOR(tc,1,t)
+#define ABS(x)          ((x)<0?-(x):(x))
+#define SQ(x)           ((x)*(x))
+#define SP(x)           fixed << setprecision(x)
+
+
+#define Make(x,p)       (x | (1<<p))
+#define DeMake(x,p)     (x & ~(1<<p))
+#define Check(x,p)      (x & (1<<p))
+#define popcount(x)     __builtin_popcount(x)
+
+int main()
+{
+      FastRead
+      TC{
+            double AB,AD,a;
+            cin >> AB >> AD >> a;
+            a=(a*PI)/180.0;
+
+            double BD=sqrt(AB*AB+AD*AD-2.0*AB*AD*cos(a));
+            double BC=2.0*BD;
+
+           // double b=asin((AD*sin(a))/BD);
+           double b=acos((SQ(AB)+SQ(BD)-SQ(AD))/(2.0*AB*BD));
+            double AC=sqrt(AB*AB+BC*BC-2.0*AB*BC*cos(b));
+            cout << SP(7) << AB*AB+AC*AC << '\n';
+      }
+}
